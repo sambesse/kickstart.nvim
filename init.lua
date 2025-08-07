00ff5cd7e65f9fe4be95321572d6a9d15db484d9
sbesse <sbesse@newagemicro.com>
2025-08-07 09:15:21 -0400

    adding some useful short cuts for coding

--- init.lua
+++ init.lua
@@ -185,7 +185,11 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n><Cmd>q<CR>', { desc = 'Exit terminal mode' })
+
+-- expedited file browsing and terminals
+vim.keymap.set('n', '<leader>b', '<Cmd>Neotree toggle<CR>')
+vim.keymap.set('n', '<leader>T', '<C-w>s<Cmd>terminal<CR>')
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
